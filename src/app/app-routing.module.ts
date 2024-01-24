bc4e8577a6fb37e0f467b5f1d86ffdcf1c0be672
AlexGerzen <121639829+AlexGerzen@users.noreply.github.com>
2024-01-24 18:20:26 +0100

    Added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FrontPageComponent } from './front-page/front-page.component';
+import { HousesComponent } from './houses/houses.component';
+import { PersonsComponent } from './persons/persons.component';
+import { QuotesComponent } from './quotes/quotes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: FrontPageComponent},
+  {path:'houses', component: HousesComponent},
+  {path:'persons', component: PersonsComponent},
+  {path:'quotes', component: QuotesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatCardModule} from '@angular/material/card';
+import { FrontPageComponent } from './front-page/front-page.component';
+import { HousesComponent } from './houses/houses.component';
+import { PersonsComponent } from './persons/persons.component';
+import { QuotesComponent } from './quotes/quotes.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrontPageComponent,
+    HousesComponent,
+    PersonsComponent,
+    QuotesComponent
   ],
   imports: [
     BrowserModule,
