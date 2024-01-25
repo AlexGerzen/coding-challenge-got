c16051341e7c5f2d5c018a3d23914517a6185a80
AlexGerzen <121639829+AlexGerzen@users.noreply.github.com>
2024-01-25 13:20:31 +0100

    Added random quotes

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./quotes.component.scss']
 })
 export class QuotesComponent {
+  url: string = "https://api.gameofthronesquotes.xyz/v1/random/5";
+  fiveRandomQuotes: any = [];
 
+  constructor() {
+    this.getFiveRandomQuotes();
+  }
+
+  /**
+   * This function will use the url an fetch the data. It throws an error when it is unsuccessful.
+   */
+  async getFiveRandomQuotes() {
+    this.fiveRandomQuotes = [];
+    await fetch(this.url)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`Error loading API data. Status code: ${response.status}`);
+        }
+
+        return response.json();
+      })
+      .then(data => {
+        this.fiveRandomQuotes.push(data);
+      })
+      .catch(error => {
+        console.error('API request error:', error);
+      });
+  }
 }
